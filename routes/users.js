625612acdc2ba0377e8a5b702a8c3ea7800c5d55
Raman Mangla <ramanmangla7@gmail.com>
2019-08-08 14:33:03 -0400

    Added password hashing and registration of user in the database

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -21,7 +22,7 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     // Checking for request data validation
     const errors = validationResult(req);
 
@@ -29,7 +30,32 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // "name" = doing "name: name" (ES6)
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // Hashing the password for encryption
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      res.send('User saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
