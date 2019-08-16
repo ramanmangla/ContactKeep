72a7d2fb615a47097fb088b0b0b19a92fbfee2cd
Raman Mangla <ramanmangla7@gmail.com>
2019-08-16 15:27:30 -0400

    Removed redundant files, added base components and CSS and added dependencies

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>ContactKeep</h1>
+      <p className='my-1'>This is a cloud contact storage application.</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
