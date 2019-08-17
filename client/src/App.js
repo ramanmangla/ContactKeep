b7448599749d71b8c8b871c69971498e946d6adb
Raman Mangla <ramanmangla7@gmail.com>
2019-08-17 16:47:38 -0400

    Created components for ContactItem and Contacts and added them to the Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import './App.css';
-import ContactState from './context/contact/ContactState';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import "./App.css";
+
+import ContactState from "./context/contact/ContactState";
 
 const App = () => {
   return (
