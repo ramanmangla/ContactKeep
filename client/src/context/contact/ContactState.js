890f5b43b20b3887559a001b7a45034214c96059
Raman Mangla <ramanmangla7@gmail.com>
2019-08-17 17:41:30 -0400

    Added context functions and reducer cases for adding and deleting contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,7 +1,7 @@
-import React, { useReducer } from 'react';
-import uuid from 'uuid';
-import ContactContext from './contactContext';
-import contactReducer from './contactReducer';
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
 import {
   ADD_CONTACT,
   DELETE_CONTACT,
@@ -10,31 +10,31 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER
-} from '../types';
+} from "../types";
 
 const ContactState = props => {
   const initialState = {
     contacts: [
       {
         id: 1,
-        name: 'Jill Johnson',
-        email: 'jill@hotmail.com',
-        phone: '111-111-232',
-        type: 'personal'
+        name: "Jill Johnson",
+        email: "jill@hotmail.com",
+        phone: "111-111-232",
+        type: "personal"
       },
       {
         id: 2,
-        name: 'Sara Watson',
-        email: 'sara@hotmail.com',
-        phone: '222-111-232',
-        type: 'personal'
+        name: "Sara Watson",
+        email: "sara@hotmail.com",
+        phone: "222-111-232",
+        type: "personal"
       },
       {
         id: 1,
-        name: 'Harry White',
-        email: 'harry@hotmail.com',
-        phone: '111-222-232',
-        type: 'professional'
+        name: "Harry White",
+        email: "harry@hotmail.com",
+        phone: "111-222-232",
+        type: "professional"
       }
     ]
   };
@@ -42,7 +42,14 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
   // Delete Contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
   // Set Current Contact
   // Clear Current Contact
   // Update Contact
@@ -52,7 +59,9 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
